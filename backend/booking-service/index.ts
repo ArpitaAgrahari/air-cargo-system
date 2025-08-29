efac2d58549884d4e225a447281dc6e621a3dfac
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-08-29 13:39:50 +0530

    Add error handling middleware to the booking service

--- backend/booking-service/index.ts
+++ backend/booking-service/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/errorHandler';
 
 
 dotenv.config();
@@ -10,6 +11,9 @@ const PORT=process.env.PORT || 5001;
 
 app.use(express.json());
 
+
+//error handling
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`Booking-service running on port: ${PORT}`);
