bfb9d469fe9e8daa6ceddc328a7b2e17e79b1741
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-08-29 13:31:31 +0530

    Initialize backend with package.json and add basic dependencies; create empty docker-compose.yaml

---
+++ backend/api/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes';
+import { errorHandler } from './middlewares/errorHandler';
+
+dotenv.config();
+
+const app=express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+//Routes
+app.use("/api",routes);
+
+
+//error-handling
+app.use(errorHandler);
+
+
+app.listen(PORT, ()=>{
+    console.log(`API running on port ${PORT}`);
+});
