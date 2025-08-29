e548a2867374a7a8642c56a20eec0950c06a33ee
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-08-30 00:06:14 +0530

    refactor: remove flight and tracking service components

--- backend/src/tracking-service/routes/trackingRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getBookingHistory } from "../controllers/trackingController";
-
-export const trackingRouter = Router();
-
-trackingRouter.get("/:ref_id/history", getBookingHistory);
