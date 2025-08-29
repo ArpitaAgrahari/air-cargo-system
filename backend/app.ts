0d616b333d3a5c61da62ed15582c24eefb1040e3
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-08-30 00:10:22 +0530

    feat: add initial Express server setup with middleware and error handling

---
+++ backend/app.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response, NextFunction } from 'express';
+import { FailedResponse } from './src/types/api.types.ts';
+import v1Routes from './src/routes/v1/index.ts';
+import authRoutes from './src/routes/auth.route.ts';
+import cors from 'cors';
+
+const app: Express = express();
+
+// Middleware
+app.use(express.json()); 
+app.use(cors());         
+
+// API Routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1', v1Routes);
+
+// Global Error Handler Middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const response: FailedResponse = {
+    success: false,
+    message: 'Internal Server Error',
+    data: null,
+    errors: { details: { message: err.message } },
+  };
+  console.error(err.stack);
+  res.status(500).json(response);
+});
+
+export default app;
