a538603cdcd2756763d8afdc3f87698e54248873
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-08-30 23:14:59 +0530

    feat: create booking page and layout with global styles and providers

---
+++ frontend/src/app/(form)/bookings/create/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BookingsForm } from "@/modules/bookings-form/index";
+
+export default function CreateBookingPage() {
+  return (
+    <div className="container mx-auto px-4 py-8 max-w-3xl">
+      <BookingsForm />
+    </div>
+  );
+}
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+import { Providers } from "@/provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ABCD - A Better Choice",
+  description: "ABCD - A Better Choice",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>{children}</Providers>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
